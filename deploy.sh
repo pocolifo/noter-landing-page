70180dbf86051e4ca86e73d6d0b4165e509ec319
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2023-07-02 10:50:02 -0400

    Whoops

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 echo "Deploying files in dist/**"
 
-ssh -i $SSH_USER@$SSH_ADDRESS 'cd html && rm -rf *'
-sftp -i $SSH_USER@$SSH_ADDRESS:/home/$SSH_USER/html <<< "put dist/**"
+ssh $SSH_USER@$SSH_ADDRESS 'cd html && rm -rf *'
+sftp $SSH_USER@$SSH_ADDRESS:/home/$SSH_USER/html <<< "put dist/**"
