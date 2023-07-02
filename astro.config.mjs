1f12f2fa9dda8f5f06a4414626d2a63b743dfd69
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2023-07-02 19:57:08 -0400

    Migrate to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-
 import solidJs from "@astrojs/solid-js";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), solidJs()]
+  integrations: [compress(), solidJs()],
+  output: "server",
+  adapter: cloudflare()
 });
